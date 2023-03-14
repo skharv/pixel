fcc0830a20c9662f345f5324a85f06be2201a3b4
Simon Harvey <simon.k.harv@gmail.com>
2023-03-14 18:18:52 +1100

    begin splitting up main and also add fps counter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use bevy::{prelude::*, window::WindowResizeConstraints};
+use bevy::{prelude::*, window::WindowResizeConstraints, diagnostic::{LogDiagnosticsPlugin, FrameTimeDiagnosticsPlugin}};
 
 use bevy_pixels::*;
-use rand::{self, Rng};
 
 mod component;
 mod bundle;
+mod system;
 
-const WIDTH: i32 = 800;
-const HEIGHT: i32 = 600;
+pub const WIDTH: i32 = 800;
+pub const HEIGHT: i32 = 600;
 
 fn main() {
     let mut app = App::new();
@@ -32,55 +32,13 @@ fn main() {
         height: HEIGHT as u32,
         ..default()
     })
-    .add_startup_system(spawn_pixel)
+    .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_startup_system(system::unit::spawn)
         .add_system_to_stage(PixelsStage::Draw, draw_background)
         .add_system_to_stage(PixelsStage::Draw, draw_pixel.after(draw_background))
-        .add_system(move_pixel)
+        .add_system(system::unit::movement)
         .run();
-}
-
-fn spawn_pixel(
-    mut command: Commands ) {
-    for _ in 0..100000 {
-        let px = rand::thread_rng().gen_range(0..WIDTH/2); 
-        let py = rand::thread_rng().gen_range(0..HEIGHT/2);
-
-        let vx = rand::thread_rng().gen_range(0..WIDTH)/10;
-        let vy = rand::thread_rng().gen_range(0..HEIGHT)/10;
-
-        let r = rand::thread_rng().gen_range(0..255);
-        let g = rand::thread_rng().gen_range(0..255);
-        let b = rand::thread_rng().gen_range(0..255);
-
-    command.spawn(bundle::PixelBundle{
-        position: component::Position {x: px as f32, y: py as f32},
-        velocity: component::Velocity {x: vx as f32, y: vy as f32},
-        colour: component::Colour { r: r, g: g, b: b, a: 255 },
-    });
-    }
-}
-
-fn move_pixel(
-    time: Res<Time>,
-    mut query: Query<(&mut component::Position, &mut component::Velocity)>
-    ) {
-    for (mut position, mut velocity) in query.iter_mut() {
-        let proposed_x = position.x + velocity.x * time.delta_seconds();
-        let proposed_y = position.y + velocity.y * time.delta_seconds();
-        
-        if proposed_x >= WIDTH as f32 || proposed_x <= 0. {
-            velocity.x = -velocity.x;
-            position.x += velocity.x * time.delta_seconds();
-        } else {
-            position.x = proposed_x;
-        }
-        if proposed_y >= HEIGHT as f32 || proposed_y <= 0. {
-            velocity.y = -velocity.y;
-            position.y += velocity.y * time.delta_seconds();
-        } else {
-            position.y = proposed_y;
-        }
-    }
 }
 
 fn draw_background(mut pixels_resource: ResMut<PixelsResource>) {
---
+++ src/system/unit.rs
@@ -0,0 +1,51 @@
+use bevy::prelude::*;
+use rand::{self, Rng};
+
+use crate::{WIDTH, HEIGHT};
+use crate::component;
+use crate::bundle;
+
+pub fn spawn(mut command: Commands ) {
+    for _ in 0..100000 {
+        let px = rand::thread_rng().gen_range(0..WIDTH/2); 
+        let py = rand::thread_rng().gen_range(0..HEIGHT/2);
+
+        let vx = rand::thread_rng().gen_range(0..WIDTH)/10;
+        let vy = rand::thread_rng().gen_range(0..HEIGHT)/10;
+
+        let r = rand::thread_rng().gen_range(0..255);
+        let g = rand::thread_rng().gen_range(0..255);
+        let b = rand::thread_rng().gen_range(0..255);
+
+    command.spawn(bundle::PixelBundle{
+        position: component::Position {x: px as f32, y: py as f32},
+        velocity: component::Velocity {x: vx as f32, y: vy as f32},
+        colour: component::Colour { r: r, g: g, b: b, a: 255 },
+    });
+    }
+}
+
+pub fn movement(
+    time: Res<Time>,
+    mut query: Query<(&mut component::Position, &mut component::Velocity)>
+    ) {
+    for (mut position, mut velocity) in query.iter_mut() {
+        let proposed_x = position.x + velocity.x * time.delta_seconds();
+        let proposed_y = position.y + velocity.y * time.delta_seconds();
+        
+        if proposed_x >= WIDTH as f32 || proposed_x <= 0. {
+            velocity.x = -velocity.x;
+            position.x += velocity.x * time.delta_seconds();
+        } else {
+            position.x = proposed_x;
+        }
+        if proposed_y >= HEIGHT as f32 || proposed_y <= 0. {
+            velocity.y = -velocity.y;
+            position.y += velocity.y * time.delta_seconds();
+        } else {
+            position.y = proposed_y;
+        }
+    }
+}
+
+
